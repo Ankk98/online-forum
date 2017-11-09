39a5d4966e34645f49781788615d5221ef41167f
ankk98 <ankk98@gmail.com>
2017-11-10 01:44:28 +0530

    initial-commit-2

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<head>
+<title>Developer Forum </title>
+</head>
+
+<body>
+<h1> My Forum</h1>
+  <div id="wrapper">
+  <div id="menu">
+      <a class="item" href="/forum/index.php"> Home </a>
+      <a class="item" href="/forum/create_topic.php"> Create a topic</a>
+      <a class="item" href="/forum/create_cat.php"> Create a category</a>
+
+  <div id="userbar">
+  <div id="userbar"> Not you? Log Out </div>
+  </div>
+  <div id="content">
